2cb9368726abd77c963b364de874acdf5d13250d
viorelaBudianu <viorela.hariga@yahoo.com>
2019-07-24 15:50:15 +0300

    prepare for Delete - schita

---
+++ ServerApp/Models/UserDeleteViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServerApp.Models
+{
+    public class UserDeleteViewModel
+    {
+        public int? Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string City { get; set; }
+
+        public string Description { get; set; }
+
+        public string Street { get; set; }
+    }
+}
